a62097a6b694713224e633a328a2f4719d75bc77
JakubZmatlo <zedkos@post.cz>
2025-04-24 18:25:21 +0200

    Font + AppContext

---
+++ RocnikovaPrace/Client/src/context/AppContext.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { createContext, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null)
+
+    const value = {navigate, user, setUser}
+
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext)
+} 

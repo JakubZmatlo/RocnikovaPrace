626707da3a8d9adeecca78299c9fb88751a3ab32
JakubZmatlo <zedkos@post.cz>
2025-07-22 14:40:40 +0200

    product model

---
+++ RocnikovaPrace/Server/models/products.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema({
+    id: { type: Number, required: true },
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    date: { type: Date, default: Date.now},
+    availability: {type: Boolean, default: true}
+
+});
+
+module.exports = mongoose.model("Product", schema);

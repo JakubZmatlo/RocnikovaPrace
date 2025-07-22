28aeac19b3ece0ba433667cb49b2b4d13c880c81
JakubZmatlo <zedkos@post.cz>
2025-07-22 18:36:55 +0200

    product controller

--- RocnikovaPrace/Server/controllers/products.js
+++ RocnikovaPrace/Server/controllers/products.js
@@ -1,42 +1,107 @@
-const Product = require('../models/products');
+const Product = require("../models/products");
 
-exports.getAllProducts = async (res, req, next) => { };
-exports.getProductById = async (res, req, next) => { };
-exports.createProduct = async (res, req, next) => {
-    let products = await Product.find({});
-    let id;
-    if (products.length > 0) {
-        let last_product_array = products.slice(-1);
-        let last_product = last_product_array[0];
-        id = last_product.id + 1;
-    }
-    else {
-        id = 1
-    }
-    try {
-        const product = new Product({
-            id: req.body.id,
-            name: req.body.name,
-            category: req.body.category,
-            price: req.body.price,
-            image: req.body.image
-        });
-        const result = await data.save();
-        if (result) {
-            return res.status(201).send({
-                message: "Product created",
-                payload: result,
-                success: true
+exports.getALLProducts = async (req, res, next) => {
+    try{
+        const data = await Product.find();
+        if (data && data.length != 0){
+            return res.status(200).send({
+                message: "Products found",
+                payload: data
             });
         }
-        res.status(500).send({
-            message: "Product was not created",
-            success: true
-        });
-    } catch (err) {
-        console.log(err);
+        res.status(404).send({
+            message: "Products were not found"
+        })
+    } catch (err){
         res.status(500).send(err);
     }
 };
-exports.updateProduct = async (res, req, next) => { };
-exports.deleteProduct = async (res, req, next) => { };
+exports.getProductById = async (req, res, next) => {
+    try{
+        const data = await Product.findById(req.params.id);
+        if (data){
+            return res.status(200).send({
+                message: "Product found",
+                payload: data
+            });
+        }
+        res.status(404).send({
+            message: "Product was not found"
+        })
+    } catch (err){
+        res.status(500).send(err);
+    }
+};
+exports.createProduct = async (req, res, next) => {
+    let products = await Product.find({});
+    let id;
+    if(products.length>0){
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }else{
+        id=1;
+    }
+    try{
+      const data = new Product({
+        id: id,
+        name: req.body.name,
+        category: req.body.category,
+        price: req.body.price,
+        image: req.body.image
+      });
+      const result = await data.save();
+      if(result){
+        return res.status(201).send({
+            success: true,
+            message: "Product created",
+            payload: result
+        });
+      }
+      res.status(500).send({
+        success:true,
+        message: "Product was not created"
+    })
+    } catch (err){
+        res.status(500).send(err);
+    }
+};
+exports.updateProduct = async (req, res, next) => {
+    try{
+        const data = {
+          id: id,
+        name: req.body.name,
+        category: req.body.category,
+        price: req.body.price,
+        image: req.body.image
+        };
+        const result = await Product.findByIdAndUpdate(req.params.id, data);
+        if(result){
+          return res.status(200).send({
+              message: "Product updated",
+              payload: result
+          });
+        }
+        res.status(500).send({
+            message: "Product was not updated"
+        })
+      } catch (err){
+          res.status(500).send(err);
+      }
+};
+exports.deleteProduct = async (req, res, next) => {
+    try{
+        const result = await  Product.findByIdAndDelete({ id: Number(req.params.id) });
+        if(result){
+          return res.status(200).send({
+              message: "Product deleted",
+              payload: result
+          });
+        }
+        res.status(500).send({
+            message: "Product was not deleted"
+        })
+      } catch (err){
+          res.status(500).send(err);
+      }
+};
